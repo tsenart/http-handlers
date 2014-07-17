c83a4d910dae918ad5d643697b39007400f7fba5
Tomás Senart <tsenart@gmail.com>
2014-07-17 21:16:29 +0200

    Refactor metrics package
    This change set refactors the metrics package and removes usage of
    global state.

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -23,78 +23,86 @@ import (
 // only the requests per second, but also the number of requests being processed
 // at any given point in time.
 func Wrap(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		atomic.AddUint64(&requests, 1)        // inc requests
-		defer atomic.AddUint64(&responses, 1) // inc responses when we're done
-		defer recordLatency(time.Now())       // record latency when we're done
+	// a five-minute window tracking 1ms-3min
+	stats := handlerStats{histogram: newHistogram(5, 1, 1000*60*3, 3)}
 
+	expvar.Publish("http", expvar.Func(func() interface{} { return stats.snapshot() }))
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		stats.requests(1)
+		defer stats.responses(1)
+		defer stats.record(time.Now())
 		h.ServeHTTP(w, r)
 	})
 }
 
-var (
-	requests, responses uint64
-	m                   sync.Mutex
-
-	// a five-minute window tracking 1ms-3min
-	latency = hdr.NewWindowedHistogram(5, 1, 1000*60*3, 3)
-)
-
-func recordLatency(start time.Time) {
-	m.Lock()
-	defer m.Unlock()
-
-	elapsedMS := time.Now().Sub(start).Seconds() * 1000.0
-	_ = latency.Current.RecordValue(int64(elapsedMS))
+// handlerStats is a container type for several HTTP handler statistics
+// NOTE(tsenart): Memory must be 64bit aligned on ARM and x86-32
+// or a runtime panic will occur in some platforms when using atomic operations.
+// See: http://golang.org/src/pkg/sync/atomic/doc.go#L52
+type handlerStats struct {
+	reqs, resps uint64
+	*histogram
 }
 
-func rotateLatency() {
-	m.Lock()
-	defer m.Unlock()
+// snapshot returns a snapshot of this handlerStats
+func (s *handlerStats) snapshot() map[string]interface{} {
+	s.Lock()
+	defer s.Unlock()
 
-	latency.Rotate()
-}
+	m := s.Merge()
 
-func getStats() httpStats {
-	req, res := atomic.LoadUint64(&requests), atomic.LoadUint64(&responses)
-
-	m.Lock()
-	defer m.Unlock()
-
-	m := latency.Merge()
-
-	return httpStats{
-		Requests:  req,
-		Responses: res,
-		Latency: latencyStats{
-			P50:  m.ValueAtQuantile(50),
-			P75:  m.ValueAtQuantile(75),
-			P90:  m.ValueAtQuantile(90),
-			P95:  m.ValueAtQuantile(95),
-			P99:  m.ValueAtQuantile(99),
-			P999: m.ValueAtQuantile(99.9),
-		},
+	return map[string]interface{}{
+		"Requests":  s.reqs,
+		"Responses": s.responses,
+		"P50":       m.ValueAtQuantile(50),
+		"P75":       m.ValueAtQuantile(75),
+		"P90":       m.ValueAtQuantile(90),
+		"P95":       m.ValueAtQuantile(95),
+		"P99":       m.ValueAtQuantile(99),
+		"P999":      m.ValueAtQuantile(999),
 	}
 }
 
-func init() {
-	go func() {
-		reset := time.NewTicker(1 * time.Minute)
-		for _ = range reset.C {
-			rotateLatency()
-		}
-	}()
-
-	expvar.Publish("http", expvar.Func(func() interface{} {
-		return getStats()
-	}))
+// reqs atomically adds n to the internal requests counter
+func (s *handlerStats) requests(n uint64) uint64 {
+	return atomic.AddUint64(&s.reqs, n)
 }
 
-type httpStats struct {
-	Requests, Responses uint64
-	Latency             latencyStats
+// resps atomically adds n to the internal responses counter
+func (s *handlerStats) responses(n uint64) uint64 {
+	return atomic.AddUint64(&s.resps, n)
 }
 
-type latencyStats struct {
-	P50, P75, P90, P95, P99, P999 int64
+// histogram is an utility type wrapping an hdr.WindowedHistogram
+type histogram struct {
+	sync.Mutex
+	*hdr.WindowedHistogram
+}
+
+func newHistogram(n int, minValue, maxValue int64, sigfigs int) *histogram {
+	h := &histogram{
+		WindowedHistogram: hdr.NewWindowedHistogram(n, minValue, maxValue, sigfigs),
+	}
+
+	go h.rotate(1 * time.Minute)
+
+	return h
+}
+
+// record safely updates the histogram's state with a new entry
+func (h *histogram) record(start time.Time) {
+	h.Lock()
+	elapsedMS := time.Now().Sub(start).Seconds() * 1000.0
+	h.Current.RecordValue(int64(elapsedMS))
+	h.Unlock()
+}
+
+// rotate safely rotates the histogram every period amount of time
+func (h *histogram) rotate(period time.Duration) {
+	for _ = range time.Tick(period) {
+		h.Lock()
+		h.Rotate()
+		h.Unlock()
+	}
 }
